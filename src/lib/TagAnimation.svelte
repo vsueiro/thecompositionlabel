88b6956f9095e8d06a5fdd7adae51c14915873e5
Vinicius Sueiro <hi@vsueiro.com>
2023-11-18 22:01:09 -0500

    Make swiper play automatically

--- src/lib/TagAnimation.svelte
+++ src/lib/TagAnimation.svelte
@@ -14,10 +14,20 @@
     border: 1px solid black;
     padding: 16px;
     width: fit-content;
+    user-select: none;
   }
 </style>
 
-<swiper-container direction="vertical" height="400" auto-height="true">
+
+<swiper-container
+  direction="vertical"
+  height="400"
+  auto-height="true"
+  autoplay-delay="1500"
+  autoplay-disable-on-interaction="false"
+  loop="true"
+  allow-touch-move="false"
+  >
   <swiper-slide>
     <div class="tag">Cool Material</div>
   </swiper-slide>
