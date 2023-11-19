6674d3c5944c0ba93577213b5b29a7e4e7efec5f
Vinicius Sueiro <hi@vsueiro.com>
2023-11-18 21:42:50 -0500

    Add base swiper for tags animations

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script>
+	import TagAnimation from "./lib/TagAnimation.svelte";
 	import FilteredList from "./lib/FilteredList.svelte";
+
+	// import function to register Swiper custom elements
+	import { register } from 'swiper/element/bundle';
+	// register Swiper custom elements
+	register();	
 </script>
 
 <main>
+	<TagAnimation />
 	<FilteredList />
 </main>
---
+++ src/lib/TagAnimation.svelte
@@ -0,0 +1,33 @@
+<style>
+  swiper-container {
+    background: Plum;
+  }
+
+  swiper-slide {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+
+  .tag {
+    background: white;
+    border: 1px solid black;
+    padding: 16px;
+    width: fit-content;
+  }
+</style>
+
+<swiper-container direction="vertical" height="400" auto-height="true">
+  <swiper-slide>
+    <div class="tag">Cool Material</div>
+  </swiper-slide>
+  <swiper-slide>
+    <div class="tag">Nicest Material</div>
+  </swiper-slide>
+  <swiper-slide>
+    <div class="tag">A Material</div>
+  </swiper-slide>
+  <swiper-slide>
+    <div class="tag">Lovely Material</div>
+  </swiper-slide>
+</swiper-container>
