4f8a2b1d03f2472688fd7b9421649b111ebcfa96
Vinicius Sueiro <hi@vsueiro.com>
2023-11-22 08:39:14 -0500

    Add colors to tags and photo overlays to show biodegradable or not (quick and dirty)

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,11 +1,37 @@
 <script>
   export let item;
+
+  // quick and dirty
+  let biodegradableMaterials = [
+    "Linen",
+    "Cotton",
+    "Viscose",
+    "Lyocell",
+    "Silk",
+    "Modal",
+  ];
+  let materials = item.Composition.split(",");
+  let biodegradableCount = 0;
+
+  for (let material of materials) {
+    if (
+      biodegradableMaterials.some((biodegradableMaterial) =>
+        material.includes(biodegradableMaterial)
+      )
+    ) {
+      biodegradableCount++;
+    }
+  }
+
+  $: isBiodegradable = materials.length === biodegradableCount;
 </script>
 
-<li class="card">
+<li class="card {isBiodegradable ? 'biodegradable' : 'non-biodegradable'}">
   <a href={item.Link} target="_blank">
     <figure>
-      <img src={item.Image} loading="lazy" />
+      <div class="photo">
+        <img src={item.Image} loading="lazy" alt={item.Title} />
+      </div>
       <figcaption>
         <div>
           <h3>{item.Title}</h3>
@@ -14,8 +40,18 @@
           {/if}
         </div>
         <ol>
-          {#each item.Composition.split(",") as item}
-            <li>{item}</li>
+          {#each item.Composition.split(",") as material}
+            <li
+              style="background: {biodegradableMaterials.some(
+                (biodegradableMaterial) =>
+                  material.includes(biodegradableMaterial)
+              )
+                ? '#bcf2ff'
+                : '#ffd7df'}
+                "
+            >
+              {material}
+            </li>
           {/each}
         </ol>
       </figcaption>
@@ -29,6 +65,8 @@
     background: #fff;
     border-radius: 8px;
     overflow: hidden;
+    outline: 1px solid transparent;
+    transition: all 0.4s;
   }
 
   a {
@@ -42,6 +80,10 @@
 
   .card:hover figcaption {
     border-color: currentColor;
+  }
+
+  .card:hover .photo:after {
+    opacity: 0.2;
   }
 
   figure {
@@ -96,5 +138,28 @@
     border-radius: 4px;
     align-self: start;
     width: fit-content;
+  }
+
+  .photo {
+    position: relative;
+  }
+
+  .photo:after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-image: radial-gradient(#ffd7df 33%, transparent 33%);
+    background-attachment: fixed;
+    background-size: 2px 2px;
+    opacity: 1;
+    transition: all 0.4s;
+  }
+
+  .biodegradable .photo:after {
+    background-image: radial-gradient(#bcf2ff 33%, transparent 33%);
   }
 </style>
