059755e60a2c47f0386477ef1dc79a7a69d6d42d
Vinicius Sueiro <hi@vsueiro.com>
2024-01-02 16:22:43 -0500

    Replace paths with new repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fast-fashion-composition/", // Repo name
+  base: "/thecompositionlabel/", // Repo name
   plugins: [svelte()],
 });
