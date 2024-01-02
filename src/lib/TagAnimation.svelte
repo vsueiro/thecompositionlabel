d1419bca9fe7a0a6e83fe177b0cd015f6c2b3550
Vinicius Sueiro <hi@vsueiro.com>
2024-01-02 01:32:00 -0500

    Apply style to animated tags on cover

--- src/lib/TagAnimation.svelte
+++ src/lib/TagAnimation.svelte
@@ -75,8 +75,8 @@
         <div class="info">
           <div class="print">
             <dt>
-              <span>{tag.name}</span>
-              <span>{tag.tagline}</span>
+              <span class="name">{tag.name}</span>
+              <span class="tagline">{tag.tagline}</span>
             </dt>
             <dd>
               <img src="assets/icons/{tag.icon}" alt="" />
@@ -84,8 +84,11 @@
                 {tag.biodegradable ? "Biodegradable" : "Non-Biodegradable"}
               </div>
               <div class="labels">
-                {#each tag.labels as label}
+                {#each tag.labels as label, index}
                   <span>{label}</span>
+                  {#if index < tag.labels.length - 1}
+                    <span aria-hidden="true">•</span>
+                  {/if}
                 {/each}
               </div>
             </dd>
@@ -100,7 +103,6 @@
   :root {
     --height: 70vh;
   }
-
   dl {
     background: linear-gradient(180deg, #c8b4d2 0%, #dfc1d0 50%, #f1e6ec 100%);
     width: 50%;
@@ -109,6 +111,42 @@
     overflow: hidden;
     flex-shrink: 0;
     border-radius: 16px;
+  }
+  dl::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    box-shadow: inset 0 0 48px #ceb8d280;
+  }
+  dd {
+    margin: 0;
+  }
+  dt {
+    position: absolute;
+    left: 80px;
+    top: 32px;
+    right: 0;
+    bottom: 32px;
+    border-left: 1px solid #494b53;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 16px;
+  }
+  .name {
+    font-family: "Dela Gothic One";
+    font-size: 24px;
+    text-transform: uppercase;
+  }
+  img {
+    position: absolute;
+    left: calc(0 + 16px);
+    top: calc(32px + 16px);
+    bottom: 32px;
+    width: 48px;
   }
   .shirt {
     height: 100%;
@@ -125,26 +163,84 @@
     display: flex;
   }
   .base {
-    width: 24px;
+    position: relative;
+    width: 32px;
     background: white;
-    border-right: 1px dashed black;
+
     transform: translate(0, var(--height));
     animation: showTagBase 6s infinite;
   }
+  .base::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 1px;
+    top: 0;
+    bottom: 0;
+    right: -1px;
+    border-right: 1px dashed white;
+  }
   .info {
-    padding: 16px;
-    min-width: 120px;
+    position: relative;
+    width: 384px;
+    height: 176px;
     background: white;
-    border-left: 1px dashed black;
+    border-left: 1px dashed white;
+    margin-left: 2px;
     transform: translate(0, var(--height));
     animation: showTagInfo 6s infinite;
   }
-  .print {
-    border: 1px solid black;
-    padding: 16px;
+  .info::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 1px;
+    top: 0;
+    bottom: 0;
+    left: -2px;
+    border-right: 1px dashed white;
   }
-  dd {
-    margin: 0;
+  .print {
+    position: relative;
+    border: 1px solid #494b53;
+    margin: 12px;
+    padding: 12px;
+    height: calc(100% - 24px);
+    border-radius: 6px;
+  }
+  .biodegradable {
+    height: 32px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+    font-size: 12px;
+    border-bottom: 1px solid #494b53;
+  }
+  .labels {
+    height: 32px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    gap: 8px;
+    letter-spacing: 0.05em;
+    font-size: 12px;
+    border-top: 1px solid #494b53;
+  }
+  .tagline {
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+    font-size: 12px;
   }
 
   @keyframes showTagBase {
