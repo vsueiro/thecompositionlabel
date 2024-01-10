9d2c749d07802c7e3aa73cb954ec6da80a078b60
Vinicius Sueiro <hi@vsueiro.com>
2024-01-09 19:10:38 -0500

    Add to previous commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/thecompositionlabel/", // Repo name
+  base: "/", // Repo name (if not custom domain)
   plugins: [svelte()],
 });
