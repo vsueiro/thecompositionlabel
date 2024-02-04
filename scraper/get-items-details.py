caa0938bd4ee8f9531a0f6d8f6e2ba04a365cb52
Vinicius Sueiro <hi@vsueiro.com>
2024-02-03 20:55:02 -0500

    Lower scraping limit

--- scraper/get-items-details.py
+++ scraper/get-items-details.py
@@ -16,7 +16,7 @@ output_folder = 'scraper/items/'
 links_folder = 'scraper/links/'
 
 # Define the amount of links per scraping batch
-limit = 300
+limit = 250
 
 # Get the most recent list of links
 def get_most_recent_csv(directory):
