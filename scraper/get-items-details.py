747b0118b42180bf9380ae84c0eb827dce771ed0
Vinicius Sueiro <hi@vsueiro.com>
2024-02-09 15:09:46 -0500

    Reduce sleep

--- scraper/get-items-details.py
+++ scraper/get-items-details.py
@@ -73,7 +73,7 @@ def get_item_details(link, item_id):
     print(f'Getting details for item {item_id}')
 
     driver.get(link)
-    time.sleep(random.uniform(6, 8))
+    time.sleep(random.uniform(1, 2))
     
     # Get hardcoded JSON
     def extract_json():
